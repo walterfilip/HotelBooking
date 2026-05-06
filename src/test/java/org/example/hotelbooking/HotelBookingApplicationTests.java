330a27897c6bfe678ad0c8d90b5852778dda8534
Filip Walter <filipwalter10@gmail.com>
2026-05-06 09:55:14 +0200

    Initial commit

---
+++ src/test/java/org/example/hotelbooking/HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hotelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
