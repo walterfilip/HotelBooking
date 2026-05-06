dfd74c980afcd4f56f3c535d263e9748965c3c76
Nissem01 <nils.modig02@gmail.com>
2026-05-06 14:28:30 +0200

    Added properties file

--- src/main/java/org/example/hotelbooking/HotelBookingApplication.java
+++ src/main/java/org/example/hotelbooking/HotelBookingApplication.java
@@ -1,12 +1,15 @@
 package org.example.hotelbooking;
 
+import org.example.hotelbooking.ui.ConsoleMenu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class HotelBookingApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         SpringApplication.run(HotelBookingApplication.class, args);
 
         ConsoleMenu consoleMenu = new ConsoleMenu();
