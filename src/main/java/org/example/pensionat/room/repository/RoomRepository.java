aa4b76a844f15ea001d495c8afd095afe46ddcd4
Stina Linde <stinizo@gmail.com>
2026-05-19 18:16:04 +0200

    Implemented available room filtering and room descriptions

--- src/main/java/org/example/pensionat/room/repository/RoomRepository.java
+++ src/main/java/org/example/pensionat/room/repository/RoomRepository.java
@@ -1,8 +1,10 @@
 package org.example.pensionat.room.repository;
 
 import org.example.pensionat.room.model.Room;
+import org.example.pensionat.room.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room,Long> {
-    int price(int price);
+   List<Room> findByRoomType(RoomType roomtype);
 }
--- src/main/java/org/example/pensionat/room/service/RoomService.java
+++ src/main/java/org/example/pensionat/room/service/RoomService.java
@@ -4,17 +4,25 @@ package org.example.pensionat.room.service;
 import org.example.pensionat.error.NotFoundException;
 import org.example.pensionat.room.model.Room;
 import org.example.pensionat.room.repository.RoomRepository;
-import org.springframework.stereotype.Service;
+import org.example.pensionat.booking.BookingStatus;
+import org.example.pensionat.booking.model.Booking;
+import org.example.pensionat.booking.repository.BookingRepository;
+import org.example.pensionat.room.RoomType;
 
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class RoomService {
 
     private final RoomRepository repository;
+    private final BookingRepository bookingRepository;
 
-    public RoomService(RoomRepository repository) {
+    public RoomService(RoomRepository repository, BookingRepository bookingRepository) {
         this.repository = repository;
+        this.bookingRepository = bookingRepository;
     }
 
     public List<Room> getAllRooms(){
@@ -24,4 +32,29 @@ public class RoomService {
     public Room getRoomById(Long id){
         return repository.findById(id).orElseThrow(() -> new NotFoundException("Kunde ej hitta rummet angivet"));
     }
+
+    public List<Room> getAvailableRooms(RoomType roomType, LocalDate startDate, LocalDate endDate){
+        List<Room> rooms = repository.findByRoomType(roomType);
+
+        return rooms.stream().filter(room -> isRoomAvailable(room, startDate, endDate)).toList();
+    }
+
+    private boolean isRoomAvailable(Room room, LocalDate startDate, LocalDate endDate) {
+        List<Booking> bookings = bookingRepository.findByRoom_IdAndStatus(
+                room.getId(),
+                BookingStatus.ACTIVE
+        );
+
+            for(Booking booking : bookings) {
+                boolean dateOverlap =
+                        !startDate.isAfter(booking.getEndDate()) &&
+                            !endDate.isBefore(booking.getStartDate());
+                if(dateOverlap) {
+                    return false;
+                }
+             }
+            return true;
+    }
+
+
 }
