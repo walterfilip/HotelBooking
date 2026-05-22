0de048f59803ca03623c79bd0a3fe0ea8eafbb01
Linda Winther <Linda.Winther@outlook.com>
2026-05-22 10:47:45 +0200

    updated todo

--- src/main/java/org/example/pensionat/PensionatApplication.java
+++ src/main/java/org/example/pensionat/PensionatApplication.java
@@ -17,43 +17,24 @@ public class PensionatApplication {
     /* Todo
     GREJER VI INTE HAR GJORT ÄN:
 
-    Registrera användare
-    Användare ska kunna ändra sina uppgifter
-    Användare ska kunna ändra sin bokning
-    Användare ska kunna ta bort  konto (Får bara tas bort om kunden inte har några aktiva bokningar kopplade till sig. Måste ha tydligt felmeddelande).
+    Registrera användare   - Nils
+    Användare ska kunna ändra sina uppgifter  - Filip
+    Användare ska kunna ändra sin bokning (Datum) - Linda
+    Användare ska kunna ta bort  konto (Får bara tas bort om kunden inte har några aktiva bokningar kopplade till sig.
+    Måste ha tydligt felmeddelande).
+
+    Startsida - knapp för logout actrive user null?
+    bekräftelse av bokning - mappa till riktig bokning
+    Knappar på startsidan, mina bokningar/min sida och logga ut
+    lägga in lösenord i SQL - Filip
+    Datumsök - Stina
 
 
-    Fortsätta med att koppla allt till frontend
-
-
-    Startsida - knapp för login, spara inloggad som bool?
-    Formulär - if inlogad inget formulär,
-    bekräftelse av bokning - gå till ens egen hemsida, lösen?
-    Display idividuella rum, lägga till beskrivning
-    Knappar på startsidan, mina bokningar och logga ut
-    löseord
-
-
-    Update login: funkar(loggas in via check mot db(user och password(just nu via phonenumber) ) kollar även boknignar mot customerid mot db och displayar
-    * kvar att göra en koppling för att hålla koll att man är inloggad
-    * eventuellt ändra meny i index så om man är inloggad kan det vara en knapp logga ut?
+    Update login: funkar(loggas in via check mot db(user och password(just nu via phonenumber)
+     ) kollar även boknignar mot customerid mot db och displayar
     * sortera bokningar i datumordning.
-    * bygga vidare på "kundsida" skapa funktioner för ändringar av kundinfo, avboka bokningar(knapp inlagd som avbokar)
-    * lägga in lösenord i SQL
-    * skapa en logout(start och i Customer
 
-
-
-    Rumsval ?
-    Bara se tillgängliga rum
-
-
-    skriva 5 enhetstester?
-
-    Linda + Filip - login
-    Stina - kopplar rum
-    Nils -
-
+    skriva 4 enhetstester?
 
      */
 }
