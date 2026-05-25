c4e3485dfa46adc84efaaae57da1bf75772e268c
Linda Winther <Linda.Winther@outlook.com>
2026-05-25 16:11:08 +0200

    done with a home button, started with a logout button

--- src/main/java/org/example/pensionat/customer/service/CustomerService.java
+++ src/main/java/org/example/pensionat/customer/service/CustomerService.java
@@ -5,6 +5,7 @@ import org.example.pensionat.booking.repository.BookingRepository;
 import org.example.pensionat.customer.model.CreateCustomerRequest;
 import org.example.pensionat.customer.model.Customer;
 import org.example.pensionat.customer.repository.CustomerRepository;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,7 +27,7 @@ public class CustomerService {
     }
 
     public Customer createCustomer(CreateCustomerRequest request) {
-        Customer customer = new  Customer(request.firstName(),request.lastName(),request.email(),request.phoneNumber(), request.password());
+        Customer customer = new Customer(request.firstName(), request.lastName(), request.email(), request.phoneNumber(), request.password());
         return customerRepository.save(customer);
     }
 
@@ -34,34 +35,34 @@ public class CustomerService {
     public boolean loginCustomer(String email, String password) {
 
 
-            if (email == null || email.isBlank()) {
-               return false;
-            }
-            if (password == null || password.isBlank()) {
-                return false;
-            }
+        if (email == null || email.isBlank()) {
+            return false;
+        }
+        if (password == null || password.isBlank()) {
+            return false;
+        }
 
-            try{
-                Customer customer = customerRepository.findByEmail(email);
-                String dbPassword = customer.getPassword();
-                if (!password.equals(dbPassword)) {
-                    System.out.println("login failed");
-                    return false;
-                }
-                if (customer == null) {
-                    return false;
-                }
-                activeCustomer = customer;
-            }catch(NullPointerException e){
-
+        try {
+            Customer customer = customerRepository.findByEmail(email);
+            String dbPassword = customer.getPassword();
+            if (!password.equals(dbPassword)) {
                 System.out.println("login failed");
                 return false;
             }
+            if (customer == null) {
+                return false;
+            }
+            activeCustomer = customer;
+        } catch (NullPointerException e) {
+
+            System.out.println("login failed");
+            return false;
+        }
 
 
         System.out.println("login successful");
-            return true;
-        }
+        return true;
+    }
 
 
     public void updateProfile(CreateCustomerRequest request) {
@@ -81,7 +82,7 @@ public class CustomerService {
     public boolean checkPassword(String password, String repeatPassword) {
 
         if (password == null || password.isBlank()) {
-         return false;
+            return false;
 
         }
         if (repeatPassword == null || repeatPassword.isBlank()) {
@@ -90,10 +91,12 @@ public class CustomerService {
         }
         Customer customer = customerRepository.findByEmail(activeCustomer.getEmail());
         if (!password.equals(customer.getPassword())) {
-            System.out.println(customer.getPassword() + " " +  password);
+            System.out.println(customer.getPassword() + " " + password);
             throw new IllegalArgumentException("passwords don't match");
 
         }
         return true;
     }
+
+
 }
