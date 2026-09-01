11860d3e849db82dc9e4e32a6a72f9a7532b0e4e
Nissem01 <nils.modig02@gmail.com>
2026-09-01 16:38:38 +0200

    rewrote test to now test a user creation returns 201. Runs against the real database and then deletes the user after test (im not sure this is how you are supposed to do it).

--- src/test/java/org/example/pensionat/customer/client/CustomerIntegrationTest.java
+++ src/test/java/org/example/pensionat/customer/client/CustomerIntegrationTest.java
@@ -1,26 +1,60 @@
 package org.example.pensionat.customer.client;
 
-
+import org.example.pensionat.customer.model.CreateCustomerRequest;
 import org.example.pensionat.customer.model.CustomerResponse;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 public class CustomerIntegrationTest {
 
-    @Autowired CustomerClient customerClient;
+    private final RestTemplate restTemplate = new RestTemplate();
 
     @Test
-    void shouldGetFirstCustomerName(){
-        CustomerResponse customer = customerClient.getCustomer(1L);
+    void registeringCustomerShouldReturn201() throws Exception {
+        CreateCustomerRequest request = new CreateCustomerRequest(
+                "Jens",
+                "Jensson",
+                "jens.test@fakemail.se",
+                "0767661111",
+                "hej"
+        );
 
-        assertNotNull(customer);
-        assertEquals("Nils", customer.firstName());
+        CustomerResponse createdCustomer = null;
 
+        try {
+            ResponseEntity<CustomerResponse> response =
+                    restTemplate.postForEntity(
+                            "http://localhost:8081/api/customers",
+                            request,
+                            CustomerResponse.class
+                    );
+
+            assertEquals(201, response.getStatusCode().value());
+
+            createdCustomer = response.getBody();
+
+            assertNotNull(createdCustomer);
+            assertTrue(createdCustomer.id() > 0);
+            assertEquals("Jens", createdCustomer.firstName());
+            assertEquals("Jensson", createdCustomer.lastName());
+            assertEquals("jens.test@fakemail.se", createdCustomer.email());
+            assertEquals("0767661111", createdCustomer.phoneNumber()
+            );
+        } finally {
+            if (createdCustomer != null) {
+                restTemplate.postForObject(
+                        "http://localhost:8081/api/customers/delete",
+                        createdCustomer.id(),
+                        String.class
+                );
+            }
+        }
     }
-
 }
